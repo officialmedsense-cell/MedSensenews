7d65554b5190d02742e1217eabf7db0741f6f81f
officialmedsense-cell <officialmedsense@gmail.com>
2026-05-10 00:17:12 -0700

    fix infinite redirect loop in next.config.js

--- next.config.js
+++ next.config.js
@@ -9,20 +9,7 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/MedSense_Dashboard',
-        destination: '/medsense_dashboard',
-        permanent: false,
-      },
-      {
-        source: '/MedSense_Dashboard/:path*',
-        destination: '/medsense_dashboard/:path*',
-        permanent: false,
-      },
-    ];
-  },
+
 }
 
 module.exports = nextConfig
