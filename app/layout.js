c6d53297a133fe7b0874ec97bde1278992034d25
officialmedsense-cell <officialmedsense@gmail.com>
2026-05-10 02:15:58 -0700

    add Google Analytics GA4 tracking (G-GFZ46JKMWT)

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,20 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="google-site-verification" content="jnE0NxZHyTJjSpUNFeEHJhnJ2GUv958hAQsRDsejMbc" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+        {/* Google Analytics GA4 */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GFZ46JKMWT"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-GFZ46JKMWT', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
