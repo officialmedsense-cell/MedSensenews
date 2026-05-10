464be943bd6288a7b2006f667211c9598717f74f
officialmedsense-cell <officialmedsense@gmail.com>
2026-05-10 01:58:41 -0700

    add google site verification tag

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="jnE0NxZHyTJjSpUNFeEHJhnJ2GUv958hAQsRDsejMbc" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
       <body>
